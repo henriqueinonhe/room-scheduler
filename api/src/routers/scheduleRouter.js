83cdf0be03cdc15a97d1484b04f15bdf78cccb01
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-01 23:24:00 -0300

    Add scheduleRouter

---
+++ api/src/routers/scheduleRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+export const scheduleRouter = express.Router();
+
+scheduleRouter.get("/users/:id/schedule", (req, res, next) => {
+  //TODO
+});
+
+scheduleRouter.get("/rooms/:id/schedule",(req, res, next) => {
+  //TODO
+});
