54494eaaf07552357a72b870e445370bb798256c
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-01 22:21:56 -0300

    Update OpenAPI documentation

--- db/init.sql
+++ db/init.sql
@@ -3,19 +3,22 @@ CREATE TABLE `Users` (
   `passwordHash` varchar(64) NOT NULL,
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `role` enum('admin','common') NOT NULL,
+  `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `Rooms` (
   `name` varchar(100) NOT NULL,
   `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4
 
 CREATE TABLE `Sessions` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `sessionId` varchar(64) NOT NULL,
   `fkUser` int unsigned NOT NULL,
+  `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `Sessions_FK` (`fkUser`),
   CONSTRAINT `Sessions_FK` FOREIGN KEY (`fkUser`) REFERENCES `Users` (`id`)
@@ -27,9 +30,10 @@ CREATE TABLE `Allocations` (
   `fkRooms` int unsigned NOT NULL,
   `startDate` datetime NOT NULL,
   `endDate` datetime NOT NULL,
+  `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `Allocations_FK` (`fkUsers`),
   KEY `Allocations_FK_1` (`fkRooms`),
   CONSTRAINT `Allocations_FK` FOREIGN KEY (`fkUsers`) REFERENCES `Users` (`id`),
   CONSTRAINT `Allocations_FK_1` FOREIGN KEY (`fkRooms`) REFERENCES `Rooms` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
