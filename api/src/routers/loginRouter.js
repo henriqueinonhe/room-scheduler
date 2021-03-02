7d5f73ee384668bfd4b87c9eaf1af99efdf2a2ea
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-01 23:14:25 -0300

    Add login router

---
+++ api/src/routers/loginRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+export const loginRouter = express.Router();
+
+loginRouter.get("/login", (req, res, next) => {
+  //TODO
+});
+
+loginRouter.get("/logout", (req, res, next) => {
+  //TODO
+});
