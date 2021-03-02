8d24caf0ede347998bc6e23b87d6623efe9984f2
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-01 23:37:00 -0300

    Add ScheduleController

---
+++ api/src/controllers/ScheduleController.js
@@ -0,0 +1,11 @@
+export class ScheduleController {
+  static async fetchUserSchedule(req, res, next) {
+    //TODO
+    next();
+  }
+
+  static async fetchRoomSchedule(req, res, next) {
+    //TODO
+    next();
+  }
+}
