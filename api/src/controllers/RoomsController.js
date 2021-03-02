7fc5edf197f5b5f00461ec2cda452e634b222838
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-01 23:35:54 -0300

    Add RoomsController

---
+++ api/src/controllers/RoomsController.js
@@ -0,0 +1,26 @@
+export class RoomsController {
+  static async fetchRooms(req, res, next) {
+    //TODO
+    next();
+  }
+
+  static async fetchSingleRoom(req, res, next) {
+    //TODO
+    next();
+  }
+
+  static async createRoom(req, res, next) {
+    //TODO
+    next();
+  }
+
+  static async updateRoom(req, res, next) {
+    //TODO
+    next();
+  }
+
+  static async deleteRoom(req, res, next) {
+    //TODO
+    next();
+  }
+}
