92b1bfc67e79e4b278132ce6137bddb5e7e8d439
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-02 20:28:35 -0300

    Fixes

--- db/init.sql
+++ db/init.sql
@@ -5,14 +5,14 @@ CREATE TABLE `Users` (
   `role` enum('admin','common') NOT NULL,
   `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Rooms` (
   `name` varchar(100) NOT NULL,
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Sessions` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
@@ -22,7 +22,7 @@ CREATE TABLE `Sessions` (
   PRIMARY KEY (`id`),
   KEY `Sessions_FK` (`fkUser`),
   CONSTRAINT `Sessions_FK` FOREIGN KEY (`fkUser`) REFERENCES `Users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Allocations` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
@@ -36,4 +36,4 @@ CREATE TABLE `Allocations` (
   KEY `Allocations_FK_1` (`fkRooms`),
   CONSTRAINT `Allocations_FK` FOREIGN KEY (`fkUsers`) REFERENCES `Users` (`id`),
   CONSTRAINT `Allocations_FK_1` FOREIGN KEY (`fkRooms`) REFERENCES `Rooms` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
