1cf736cb6b03ac01f23b19c2ec79a0e4cb068623
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-03-07 17:54:45 -0300

    Write AllocationsController skeleton

---
+++ api/src/controllers/AllocationsController.js
@@ -0,0 +1,27 @@
+import { controllerMethodWrapper } from "../helpers/controllerHelper";
+
+export class AllocationsController {
+  static fetchAllocations = controllerMethodWrapper(async (req, res, next) => {
+    //TODO
+  });
+
+  static fetchSingleAllocation = controllerMethodWrapper(async (req, res, next) => {
+    //TODO
+  });
+
+  static createAllocation = controllerMethodWrapper(async (req, res, next) => {
+    //TODO
+  });
+
+  static deleteAllocation = controllerMethodWrapper(async (req, res, next) => {
+    //TODO
+  });
+
+  static fetchUserAllocations = controllerMethodWrapper(async (req, res, next) => {
+    //TODO
+  });
+
+  static fetchRoomAllocations = controllerMethodWrapper(async (req, res, next) => {
+    //TODO
+  });
+}
